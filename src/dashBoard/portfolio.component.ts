42fce6f1bedd32c251cc3e58ba3c4292c2217088
AnandSant <AnandSantDev@gmail.com>
2019-11-21 22:18:21 -0500

    Component creation and interpolation

---
+++ src/dashBoard/portfolio.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'portfolio-home',
+	templateUrl: './portfolio.component.html'
+})
+
+export class PortfolioComponent{
+	portfolioName: string = "Anand Kumar Sant";
+	protfolioRole: string = "Application Developer";
+}
---
+++ src/employee/employee.component.ts
@@ -0,0 +1,10 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+// 	selector: 'employee-home',
+// 	template: '{{pageTitle}}'	
+// })
+
+// export class employeeComponent{
+// 	pageTitle: string = 'Employee'
+// }
